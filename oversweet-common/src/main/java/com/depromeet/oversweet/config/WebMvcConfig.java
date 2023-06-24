baa52d3cdf374cf93c174bb0b93d001325f64bc0
soochan <tncksdl05@gmail.com>
2023-06-24 13:12:57 +0900

    feature: cors 설정 추가 (#10)

---
+++ oversweet-common/src/main/java/com/depromeet/oversweet/config/WebMvcConfig.java
@@ -0,0 +1,33 @@
+package com.depromeet.oversweet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 프론트의 도메인 주소를 추후에 설정할 수 있음
+                // Set the list of headers that a pre-flight request can list as allowed for use during an actual request.
+                .allowedHeaders("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                // allow to use cookies (쿠키 사용을 허용할지 여부) : cookie에 저장된 refresh token을 사용하기 위해 필요
+                .allowCredentials(true)
+                // allow to read response Authorization header
+                .exposedHeaders("Authorization", "Set-Cookie");
+
+    }
+}
