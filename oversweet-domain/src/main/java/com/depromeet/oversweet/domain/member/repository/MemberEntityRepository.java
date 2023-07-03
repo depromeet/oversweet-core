2651c767e9a703db254de32b72e7dfbef78e337c
parkje0927 <parkje09270@gmail.com>
2023-07-03 22:44:00 +0900

    remove: OpenFeign 설정 오류로 1차 커밋 진행했던 파일 삭제 (#17)

--- oversweet-domain/src/main/java/com/depromeet/oversweet/domain/member/repository/MemberEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.depromeet.oversweet.domain.member.repository;
-
-import com.depromeet.oversweet.domain.member.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberEntityRepository extends JpaRepository<MemberEntity, Long> {
-
-}
