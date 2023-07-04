9a7d4595d28d3717d9cca6f5135fd13fdc8617ef
parkje0927 <parkje09270@gmail.com>
2023-07-04 23:05:21 +0900

    feat: 추가 정보 기입 후 토큰 생성을 위해 reponse 에 id 도 추가 - PR 리뷰 반영 (#17)

--- oversweet-api/src/main/java/com/depromeet/oversweet/member/dto/SignUpResponse.java
+++ oversweet-api/src/main/java/com/depromeet/oversweet/member/dto/SignUpResponse.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 @Builder
 public record SignUpResponse(
 
+        @Schema(description = "멤버 고유 ID", example = "1") Long id,
+
         @Schema(description = "하루 권장 당 섭취량", example = "25") Integer dailySugar
 ) {
 }
