b462571f9c20c80707fe45d2be329769812f2108
parkje0927 <parkje09270@gmail.com>
2023-07-04 23:02:54 +0900

    chore: 엔드포인트 변경, 토큰 반환 시 HttpHeaders 활용 - PR 리뷰 반영 (#17)

--- oversweet-api/src/main/java/com/depromeet/oversweet/member/controller/MemberController.java
+++ oversweet-api/src/main/java/com/depromeet/oversweet/member/controller/MemberController.java
@@ -1,23 +1,22 @@
 package com.depromeet.oversweet.member.controller;
 
 import com.depromeet.oversweet.member.dto.KakaoTokenRequest;
-import com.depromeet.oversweet.member.dto.NicknameValidationResponse;
 import com.depromeet.oversweet.member.dto.SignUpRequest;
 import com.depromeet.oversweet.member.dto.SignUpResponse;
 import com.depromeet.oversweet.member.dto.SocialSignInResponse;
 import com.depromeet.oversweet.member.service.MemberFacade;
 import com.depromeet.oversweet.response.DataResponse;
+import com.depromeet.oversweet.response.MessageResponse;
 import com.depromeet.oversweet.util.JwtTokenProvider;
 import com.depromeet.oversweet.util.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
 @Slf4j
 @Tag(name = "회원", description = "회원 관련 API")
@@ -39,27 +39,28 @@ public class MemberController {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Operation(summary = "닉네임 중복 체크", description = "닉네임 중복 체크 API 입니다.")
-    @ApiResponses({@ApiResponse(responseCode = "200", description = "닉네임 중복 체크 성공")})
-    @PostMapping("/validation")
-    public ResponseEntity<DataResponse<NicknameValidationResponse>> checkNicknameDuplicated(@RequestParam String nickname) {
-        NicknameValidationResponse validationResponse = memberFacade.checkNicknameDuplicated(nickname);
-        return ResponseEntity.ok().body(DataResponse.of(HttpStatus.OK, "닉네임 중복 체크 성공", validationResponse));
+    @ApiResponses({@ApiResponse(responseCode = "200", description = "닉네임 사용 가능")})
+    @PostMapping("/validation/nickname")
+    public ResponseEntity<MessageResponse> checkNicknameDuplicated(@RequestParam String nickname) {
+        memberFacade.checkNicknameDuplicated(nickname);
+        return ResponseEntity.ok().body(MessageResponse.of(HttpStatus.OK, "닉네임 사용 가능"));
     }
 
     @Operation(summary = "카카오 회원가입 및 로그인 성공", description = "카카오 로그인 API 입니다.")
     @ApiResponses({@ApiResponse(responseCode = "200", description = "카카오 회원가입 및 로그인 성공")})
     @PostMapping("/kakao")
-    public ResponseEntity<DataResponse<SocialSignInResponse>> kakaoSignIn(@RequestBody final KakaoTokenRequest kakaoTokenRequest,
-                                                                          HttpServletResponse httpServletResponse) {
+    public ResponseEntity<DataResponse<SocialSignInResponse>> kakaoSignIn(@RequestBody final KakaoTokenRequest kakaoTokenRequest) {
         SocialSignInResponse socialSignInResponse = memberFacade.signInKakao(kakaoTokenRequest);
+        HttpHeaders headers = new HttpHeaders();
 
-        if (!socialSignInResponse.isNewMember()) {
+        if (socialSignInResponse.isRequiredInfoExist()) {
             TokenResponse tokenResponse = jwtTokenProvider.generateJwtToken(socialSignInResponse.id());
-            httpServletResponse.setHeader(AUTHORIZATION, tokenResponse.accessToken());
-            httpServletResponse.addCookie(new Cookie("refreshToken", tokenResponse.refreshToken()));
+            headers.add(AUTHORIZATION, "Bearer " + tokenResponse.accessToken());
+            headers.add(SET_COOKIE, tokenResponse.refreshToken());
         }
 
-        return ResponseEntity.ok().body(DataResponse.of(HttpStatus.OK, "카카오 회원가입 및 로그인 성공", socialSignInResponse));
+        return new ResponseEntity<>(DataResponse.of(HttpStatus.CREATED, "카카오 회원가입 및 로그인 성공", socialSignInResponse),
+                headers, HttpStatus.CREATED);
     }
 
     @Operation(summary = "추가 정보 기입 후 회원가입", description = "추가 정보 기입 후 회원가입 API 입니다.")
@@ -67,7 +68,14 @@ public class MemberController {
     @PostMapping("/auth")
     public ResponseEntity<DataResponse<SignUpResponse>> signUp(@RequestBody @Valid final SignUpRequest signUpRequest) {
         SignUpResponse signUpResponse = memberFacade.signUp(signUpRequest);
-        return ResponseEntity.ok().body(DataResponse.of(HttpStatus.OK, "추가 정보 기입 후 회원가입 성공", signUpResponse));
+        TokenResponse tokenResponse = jwtTokenProvider.generateJwtToken(signUpResponse.id());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(AUTHORIZATION, "Bearer " + tokenResponse.accessToken());
+        headers.add(SET_COOKIE, tokenResponse.refreshToken());
+
+        return new ResponseEntity<>(DataResponse.of(HttpStatus.CREATED, "추가 정보 기입 후 회원가입 성공", signUpResponse),
+                headers, HttpStatus.CREATED);
     }
 
 }
