024ea16812f71bf6c4dfcb105b2ac34cbd56cec5
parkje0927 <parkje09270@gmail.com>
2023-07-04 23:00:21 +0900

    chore: 필수 정보를 기입했는지 여부를 판단하는 필드명 변경 - PR 리뷰 반영 (#17)

--- oversweet-api/src/main/java/com/depromeet/oversweet/member/dto/SocialSignInResponse.java
+++ oversweet-api/src/main/java/com/depromeet/oversweet/member/dto/SocialSignInResponse.java
@@ -8,11 +8,11 @@ public record SocialSignInResponse(
 
         @Schema(description = "회원 고유 ID", example = "1") Long id,
 
-        @Schema(description = "신규 회원 여부", example = "true") boolean isNewMember
+        @Schema(description = "필수 정보 기입 여부", example = "true") boolean isRequiredInfoExist
 
 ) {
-    public SocialSignInResponse(final Long id, final boolean isNewMember) {
+    public SocialSignInResponse(final Long id, final boolean isRequiredInfoExist) {
         this.id = id;
-        this.isNewMember = isNewMember;
+        this.isRequiredInfoExist = isRequiredInfoExist;
     }
 }
