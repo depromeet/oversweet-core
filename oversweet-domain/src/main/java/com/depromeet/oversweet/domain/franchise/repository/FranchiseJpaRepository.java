015111eb626c990d1e0fe19b7062d40fd8bf9cc9
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2023-07-07 15:37:07 +0900

    fix: PR 리뷰 반영

--- oversweet-domain/src/main/java/com/depromeet/oversweet/domain/franchise/repository/FranchiseJpaRepository.java
+++ oversweet-domain/src/main/java/com/depromeet/oversweet/domain/franchise/repository/FranchiseJpaRepository.java
@@ -1,8 +1,10 @@
 package com.depromeet.oversweet.domain.franchise.repository;
 
 import com.depromeet.oversweet.domain.franchise.entity.FranchiseEntity;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -10,5 +12,6 @@ import java.util.List;
 public interface FranchiseJpaRepository extends JpaRepository<FranchiseEntity, Long>, FranchiseEntityFilter {
 
     @Query(value = "select * from franchise f where f.name like %:keyword%", nativeQuery = true)
+    @QueryHints(@QueryHint(name = "org.hibernate.readOnly", value = "true"))
     List<FranchiseEntity> findByKeyword(@Param("keyword") String keyword);
 }
